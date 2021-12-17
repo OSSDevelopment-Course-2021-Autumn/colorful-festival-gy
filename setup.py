050ab558fad541cd901ed4ee65464407f6424813
Guyutongxue <guyutongxue@163.com>
2021-12-17 17:15:52 +0800

    upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "colorful-festival-gy",
+    version = "0.1.2",
+    author = "Guyutongxue",
+    author_email = "guyutongxue@163.com",
+    description = "An OSS course practice",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/OSSDevelopment-Course-2021-Autumn/colorful-festival-gy",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    package_dir = {
+       "": "src"
+    },
+    packages = find_packages(where="src"),
+    python_requires = ">=3.6"
+)
